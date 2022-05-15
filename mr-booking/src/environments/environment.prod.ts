75db36b434f6836116e5c026149c1d9e84f9822e
Roman Baklanov <wackier.leaf@yandex.ru>
2022-05-15 20:24:37 +0300

    Initial

---
+++ mr-booking/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    projectId: 'mr-b00king',
+    appId: '1:712018421739:web:b4e6bc59b975cea4381bae',
+    storageBucket: 'mr-b00king.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyCIcizg_LZbD5m6nkfGs9HrZJGucTyLRtI',
+    authDomain: 'mr-b00king.firebaseapp.com',
+    messagingSenderId: '712018421739',
+  }
+};
---
+++ mr-booking/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    projectId: 'mr-b00king',
+    appId: '1:712018421739:web:b4e6bc59b975cea4381bae',
+    storageBucket: 'mr-b00king.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyCIcizg_LZbD5m6nkfGs9HrZJGucTyLRtI',
+    authDomain: 'mr-b00king.firebaseapp.com',
+    messagingSenderId: '712018421739',
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
