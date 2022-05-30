4a3d77f5265b9480af7547b9004fd41a8c3aa237
Roman Baklanov <wackier.leaf@yandex.ru>
2022-05-30 18:07:34 +0300

    Добавлен аватар пользователя в header

--- mr-booking/src/app/toolbar/header/header.component.ts
+++ mr-booking/src/app/toolbar/header/header.component.ts
@@ -1,20 +1,38 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component, EventEmitter, OnDestroy, OnInit, Output} from '@angular/core';
 import {TranslateService} from "@ngx-translate/core";
 import {marker as _} from '@biesbjerg/ngx-translate-extract-marker';
+import {UserService} from "../../shared/services/user.service";
+import {Observable, Subscription} from "rxjs";
+import {AuthService} from "../../shared/services/auth.service";
+import {User} from "../../shared/services/user";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy{
   @Output() sidenavOpen: EventEmitter<any> = new EventEmitter<any>()
+  user$: Observable<User>
 
   isRuLang = true;
 
+  private readonly subList$ = new Subscription()
   constructor(
     private readonly translationService: TranslateService,
+    private readonly userService: UserService,
+    private readonly authService: AuthService
   ) { }
+
+  ngOnInit() {
+    this.subList$.add(
+      this.authService.afAuth.authState.subscribe(data => {
+        if(data){
+          this.user$ = this.userService.getUser(data.uid) as Observable<User>
+        }
+      })
+    )
+  }
 
   get translationKey() {
     return this.isRuLang ? _('MAIN.LANGUAGES.RUSSIAN') : _('MAIN.LANGUAGES.ENGLISH');
@@ -29,4 +47,7 @@ export class HeaderComponent {
     this.sidenavOpen.emit()
   }
 
+  ngOnDestroy() {
+    this.subList$.unsubscribe()
+  }
 }
--- mr-booking/src/app/toolbar/menu/menu.component.ts
+++ mr-booking/src/app/toolbar/menu/menu.component.ts
@@ -1,34 +1,17 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {AuthService} from "../../shared/services/auth.service";
-import {UserService} from "../../shared/services/user.service";
-import {map, Observable} from "rxjs";
+import {User} from "../../shared/services/user";
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
-export class MenuComponent implements OnInit {
-  userName: Observable<string>
+export class MenuComponent {
+  @Input() userData: User | null
 
   constructor(
     public readonly authService: AuthService,
-    private readonly userService: UserService
-  ) {
-
-
-    this.authService.afAuth.authState.subscribe(data => {
-        if (data) {
-          this.userName = this.userService.getUser(data.uid).pipe(
-            // @ts-ignore
-            map(user => user.displayName as string)
-          )
-        }
-      }
-    )
-  }
-
-  ngOnInit(): void {
-  }
+  ) {}
 
 }
